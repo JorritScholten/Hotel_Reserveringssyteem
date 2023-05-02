d3e14b25abb5c6ad765ac650a20b310d64c23305
ccmar <ccmdehoop1991@gmail.com>
2023-05-02 16:00:44 +0200

    Basic panel

--- src/gui/Menu.java
+++ src/gui/Menu.java
@@ -5,59 +5,41 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Menu {
-    static JButton button;
-    static JFrame mainFrame;
-    static JTextField textField;
+public class Menu extends JFrame {
 
 
-
-    public static void main(String[] args) {
-        startMenu();
+    public Menu() {
+        this.setSize(720, 500);
+        this.setResizable(false);
+        this.setLayout(null);
+        this.getContentPane().setBackground(Color.GRAY);
+        button(this, 50, 400, "decline");
+        button(this, 500, 400, "accept");
+        textField(this, 200, 50,"Surname");
+        textField(this, 400, 50,"Forname");
     }
 
-    public static void startMenu() {
-        mainFrame();
-        button(mainFrame,50,400,"decline");
-        button(mainFrame,500,400,"accept");
-        textFieldSurname(mainFrame,50,250);
 
-    }
-
-    private static void mainFrame(){
-        mainFrame = new JFrame();
-        mainFrame.setSize(720, 500);
-        mainFrame.setResizable(false);
-        mainFrame.setVisible(true);
-        mainFrame.getContentPane().setBackground(Color.GRAY);
-    }
-
-    private static void button(JFrame frame,int locationX,int locationY,String buttonName ){
-        button = new JButton();
-        ActionListener MouseListener = button.getAction();
-        button.setSize(160,40);
-        button.setLocation(locationX,locationY);
+    private static void button(JFrame frame, int locationX, int locationY, String buttonName) {
+        JButton button = new JButton();
+        button.setSize(160, 40);
+        button.setLocation(locationX, locationY);
         button.setBackground(Color.white);
         button.setText(buttonName);
-        button.addActionListener(MouseListener);
-        button.setVisible(true);
         frame.add(button);
     }
 
-    private static void textFieldSurname(JFrame frame, int locationX, int locationY){
-        textField = new JTextField();
-        textField.setSize(160,40);
-        textField.setLocation(locationX,locationY);
+    private static void textField(JFrame frame, int locationX, int locationY,String name) {
+        JTextField textField = new JTextField();
+        textField.setSize(100, 50);
+        textField.setFont(new Font("Consolas", Font.PLAIN, 35));
+        textField.setForeground(Color.white);
         textField.setBackground(Color.white);
-        textField.setText("Surname");
-        textField.setVisible(true);
+        textField.setLocation(locationX, locationY);
+        textField.setCaretColor(Color.BLACK);
+        textField.setName(name);
         frame.add(textField);
     }
 
-    private void buttonClicked(ActionEvent e) {
-        if (e.getSource() == button) {
-
-        }
-    }
 
 }
