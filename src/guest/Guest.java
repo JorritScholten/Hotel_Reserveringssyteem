042cee69b1ffa07ff4978b59d91d98d736851b6b
David <davidmeijer94@gmail.com>
2023-05-02 15:10:18 +0200

    Made changes according to comments

--- src/guest/Guest.java
+++ src/guest/Guest.java
@@ -5,13 +5,9 @@ import java.util.Date;
 import java.util.List;
 
 public class Guest {
-    private String name;
+    final private String name;
 
     public Guest(String name) {
-        this.name = name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 
