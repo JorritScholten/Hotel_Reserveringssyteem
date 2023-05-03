9b8d2233568b89755aa023a027df154a9f748409
avd-web <avd-web@outlook.com>
2023-05-03 12:02:13 +0200

    Added toString method for rooms

--- src/room/SingleBedroom.java
+++ src/room/SingleBedroom.java
@@ -4,4 +4,9 @@ public class SingleBedroom extends BookableRoom {
     public SingleBedroom() {
         super();
     }
+
+    @Override
+    public String toString(){
+        return "Single bedroom, ID:" + getRoomNumber();
+    }
 }
--- src/room/Toilet.java
+++ src/room/Toilet.java
@@ -4,4 +4,9 @@ public class Toilet extends Room{
     public Toilet() {
         super();
     }
+
+    @Override
+    public String toString(){
+        return "Toilet, ID:" + getRoomNumber();
+    }
 }
